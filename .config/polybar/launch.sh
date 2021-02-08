06009bf870cc852bc86e2227629bf63bdb7e61fb
bazeeel <deakfefe77@gmail.com>
2021-02-08 05:08:58 +0100

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 	MONITOR=$m polybar --reload mainbar-bspwm & 
 	 
 done
